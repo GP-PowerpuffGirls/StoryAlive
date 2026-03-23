81a495c68b56a47785b5508bf45cac255e46084e
Menna Mohamed <2022170446@cis.asu.edu.eg>
2026-03-24 01:39:31 +0200

    add API for Enums

---
+++ Backend/StoryAlive/StoryAlive/src/main/kotlin/com/StoryAlive/StoryAlive/Controllers/EnumsController.kt
@@ -0,0 +1,32 @@
+package com.StoryAlive.StoryAlive.Controllers
+
+import com.StoryAlive.StoryAlive.Enums.Genre
+import com.StoryAlive.StoryAlive.Enums.Tags
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("enums")
+class EnumsController {
+
+    @GetMapping("/all")
+    fun getAllEnums(): Map<String, List<String>> {
+        return mapOf(
+            "genre" to Genre.entries.map { it.name },
+            "tags" to Tags.entries.map { it.name }
+        )
+    }
+
+//    Response Example
+//    {
+//        "genre": [
+//            "COMEDY",
+//            "ACTION"
+//        ],
+//            "tags": [
+//            "HORROR",
+//        ]
+//    }
+
+}
