7ab2d786b4f89b5bbacb82871c617e9b14a264bf
mariamelkondakly <mariamelkondakly88@gmail.com>
2026-06-21 15:55:33 +0300

    Update TTSResponse.kt

--- Backend/StoryAlive/StoryAlive/src/main/kotlin/com/StoryAlive/StoryAlive/DTOs/Story/TTSResponse.kt
+++ Backend/StoryAlive/StoryAlive/src/main/kotlin/com/StoryAlive/StoryAlive/DTOs/Story/TTSResponse.kt
@@ -3,5 +3,6 @@ package com.StoryAlive.StoryAlive.DTOs.Story
 public data class TTSResponse(
     val fileName: String,
     val duration: Double,
-    val audioPath: String
+    val audioPath: String,
+    val storyCreationDTO: StoryCreationDTO
 )
